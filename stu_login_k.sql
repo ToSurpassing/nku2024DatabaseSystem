c0c4dcac7bc680ab1ca7e4efeb5d26dd5e531ef6
ToSurpassing <132668174+ToSurpassing@users.noreply.github.com>
2024-06-08 20:13:55 +0800

    Delete stu_login_k.sql

--- stu_login_k.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `stu_login_k` (`stu_id`, `stu_pass`) VALUES ('182011011', '123456');
-INSERT INTO `stu_login_k` (`stu_id`, `stu_pass`) VALUES ('182085211', '123456');
-INSERT INTO `stu_login_k` (`stu_id`, `stu_pass`) VALUES ('182085212', '123456');
-INSERT INTO `stu_login_k` (`stu_id`, `stu_pass`) VALUES ('182085215', '123456');
-INSERT INTO `stu_login_k` (`stu_id`, `stu_pass`) VALUES ('206546845', '123456');
-INSERT INTO `stu_login_k` (`stu_id`, `stu_pass`) VALUES ('209045599', '123456');
-INSERT INTO `stu_login_k` (`stu_id`, `stu_pass`) VALUES ('209055599', '123456');
-INSERT INTO `stu_login_k` (`stu_id`, `stu_pass`) VALUES ('209084163', '123456');
-INSERT INTO `stu_login_k` (`stu_id`, `stu_pass`) VALUES ('209114122', '654321');
